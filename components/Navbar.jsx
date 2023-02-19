5a8470a607506499a33ba01b5e8908726dc26f5e
LucidJoy <jyotirmoykarmakar02@gmail.com>
2023-02-19 13:28:03 +0530

    dynamic btns

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,6 +27,14 @@ const Navbar = () => {
   const { currentAccount } = useContext(DivvyContext);
   const { selectedBtn, setSelectedBtn } = useContext(NFTRentContext);
   let admin = true;
+  const router = useRouter();
+  const param = router.pathname;
+
+  useEffect(() => {
+    if (param === "/") {
+      setSelectedBtn("");
+    }
+  }, []);
 
   return (
     <nav className='flex items-center justify-around w-screen fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1'>
@@ -47,7 +55,10 @@ const Navbar = () => {
           className={`font-poppins text-[15px] transition-all duration-150 ease-in-out hover:scale-[1.05] ${
             selectedBtn === "marketplace" && "focus:text-[#eb7d42]"
           }`}
-          onClick={() => setSelectedBtn("marketplace")}
+          onClick={() => {
+            setSelectedBtn("marketplace");
+            router.push("/marketplace");
+          }}
         >
           <p>Marketplace</p>
         </button>
@@ -56,7 +67,10 @@ const Navbar = () => {
           className={`font-poppins text-[15px] transition-all duration-150 ease-in-out hover:scale-[1.05] ${
             selectedBtn === "mynfts" && "focus:text-[#eb7d42]"
           }`}
-          onClick={() => setSelectedBtn("mynfts")}
+          onClick={() => {
+            setSelectedBtn("mynfts");
+            router.push("/mynfts");
+          }}
         >
           <p>My NFTs</p>
         </button>
@@ -65,7 +79,10 @@ const Navbar = () => {
           className={`font-poppins text-[15px] transition-all duration-150 ease-in-out hover:scale-[1.05] ${
             selectedBtn === "rentednfts" && "focus:text-[#eb7d42]"
           }`}
-          onClick={() => setSelectedBtn("rentednfts")}
+          onClick={() => {
+            setSelectedBtn("rentednfts");
+            router.push("/rentednfts");
+          }}
         >
           <p>Rented NFTs</p>
         </button>
---
+++ pages/rentednfts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const rentednfts = () => {
+  return <div>rentednfts</div>;
+};
+
+export default rentednfts;
